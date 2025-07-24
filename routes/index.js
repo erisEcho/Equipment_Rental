b6abfb3cbd21b734a646768a786407d866a0e94f
erisEcho <22258361@life.hkbu.edu.hk>
2025-07-24 14:47:06 +0800

    part1

--- routes/index.js
+++ routes/index.js
@@ -44,34 +44,43 @@ router.get('/equipment/add', function(req, res) {
 });
 
 
-/* GET edit page. */
-router.get('/equipment/edit', async function (req, res) {
+// display the update form
+router.get('/equipment/edit/:id', async function (req, res) {
   const db = await connectToDB();
   try {
-    let result = await db.collection("equpiments").find().toArray();
-    res.render('equpiments', { equpiments: result });
-  } catch (error) {
-    res.status(400).json({ error: error.message });
+    let result = await db.collection("equipments").findOne({ _id: new ObjectId(req.params.id) });
+    if (result) {
+      res.render('edit', { equipment: result });
+    } else {
+      res.status(404).json({ message: "equipment not found" });
+    }
+  } catch (err) {
+    res.status(400).json({ message: err.message });
   } finally {
     await db.client.close();
   }
-})
+});
 
 // post part -------
 
-/* POST Rental Equipment */
-router.post('/equipment/add', async function(req, res){
+/* POST Add Equipment */
+router.post('/equipment/add', async function(req, res) {
   const db = await connectToDB();
-  try{
-    // 待修改
-    req.body.numTickets = parseInt(req.body.numTickets);
-    req.body.terms = req.body.terms;
-    req.body.created_at = new Date();
-    req.body.modified_at = new Date();
+  try {
+    // 构造要插入的设备对象
+    req.body.name=req.body.name;
+    req.body.description=req.body.description;
+    req.body.imageURL=req.body.imageURL;
+    req.body.contactPerson=req.body.contactPerson;
+    req.body.color=req.body.color;
+    req.body.highlighted=req.body.highlighted === 'on';
+    req.body.created_at=new Date();
+    req.body.modified_at=new Date();
 
-    let result = await db.collection("equipments").insertOne(req.body);
-    res.status(201).json({ id: result.insertedId
-    });
+
+    await db.collection("equipments").insertOne(req.body);
+
+    res.redirect('/equipments');
   } catch (error) {
     res.status(400).json({ error: error.message });
   } finally {
@@ -86,6 +95,52 @@ router.post('/equipment/delete/:id', async function (req, res) {
     let result = await db.collection("equipments").deleteOne({ _id: new ObjectId(req.params.id) });
     if (result.deletedCount > 0) {
       res.status(200).json({ message: "equipment deleted" });
+    } else {
+      res.status(404).json({ message: "equipment not found" });
+    }
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  } finally {
+    await db.client.close();
+  }
+});
+
+
+// Delete a single Booking
+// deleteCOunt 检测删除情况
+router.post('/equipment/delete/:id', async function (req, res) {
+  const db = await connectToDB();
+  try {
+    let result = await db.collection("equipments").deleteOne({ _id: new ObjectId(req.params.id) });
+    if (result.deletedCount > 0) {
+      res.status(200).json({ message: "equipment deleted" });
+    } else {
+      res.status(404).json({ message: "equipment not found" });
+    }
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  } finally {
+    await db.client.close();
+  }
+});
+
+// Update a single Equipment
+router.post('/equipment/update/:id', async function (req, res) {
+  const db = await connectToDB();
+  try {
+    req.body.name=req.body.name;
+    req.body.description=req.body.description;
+    req.body.imageURL=req.body.imageURL;
+    req.body.contactPerson=req.body.contactPerson;
+    req.body.color=req.body.color;
+    req.body.highlighted=req.body.highlighted === 'on';
+
+    req.body.modified_at=new Date();
+
+    let result = await db.collection("equipments").updateOne({ _id: new ObjectId(req.params.id) }, { $set: req.body });
+
+    if (result.modifiedCount > 0) {
+      res.status(200).json({ message: "equipment updated" });
     } else {
       res.status(404).json({ message: "equipment not found" });
     }
